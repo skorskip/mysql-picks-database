0cdef70d8188184309f3c243d1074feba67a86f1
skorskip <skorskip@umich.edu>
2019-10-03 21:35:35 -0400

    Added users and picks

--- init_db.sql
+++ init_db.sql
@@ -6,27 +6,70 @@ ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'p1cks4ppDB';
 FLUSH PRIVILEGES;
 
 CREATE TABLE teams (
-id INT PRIMARY KEY,
-primaryColor VARCHAR(20),
-secondaryColor VARCHAR(20),
-name VARCHAR(20),
-abbreviation VARCHAR(10)
+    teamId INT PRIMARY KEY,
+    primaryColor VARCHAR(20),
+    secondaryColor VARCHAR(20),
+    name VARCHAR(20),
+    abbreviation VARCHAR(10)
 );
 
 CREATE TABLE games (
-id INT,
-lastUpdated DATETIME,
-week INTEGER,
-season INTEGER,
-date DATETIME,
-submitDate DATETIME,
-homeTeam INT,
-awayTeam INT,
-homeScore INT,
-awayScore INT,
-spread INTEGER,
-progress VARCHAR(20),
-isOn BOOLEAN
+    gameId INT PRIMARY KEY,
+    lastUpdated DATETIME,
+    week INTEGER,
+    season INTEGER,
+    date DATETIME,
+    submitDate DATETIME,
+    homeTeam INT,
+    awayTeam INT,
+    homeScore INT,
+    awayScore INT,
+    spread INTEGER,
+    status VARCHAR(20),
+    isOn BOOLEAN
+);
+
+CREATE TABLE users (
+    userId INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20),
+    password VARCHAR(50),
+    email VARCHAR(50),
+
+    CONSTRAINT UC_user UNIQUE (name),
+    CONSTRAINT UC_email UNIQUE (email)
+);
+
+CREATE TABLE picks (
+    pickId INT PRIMARY KEY AUTO_INCREMENT,
+    season INT,
+    week INT,
+    gameId INT,
+    teamId INT,
+    userId INT,
+    submitted DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated DATETIME ON UPDATE CURRENT_TIMESTAMP,
+
+    CONSTRAINT UC_pick UNIQUE (gameId, week, season),
+
+    INDEX game_ind (gameId),
+    INDEX team_ind (teamId),
+    INDEX user_ind (userId),
+
+    FOREIGN KEY (teamId)
+        REFERENCES teams(teamId)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+
+    FOREIGN KEY (gameId)
+        REFERENCES games(gameId)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+
+    FOREIGN KEY (userId)
+        REFERENCES users(userId)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+
 );
 
 LOAD DATA INFILE '/var/lib/mysql-files/teamData.csv' 
